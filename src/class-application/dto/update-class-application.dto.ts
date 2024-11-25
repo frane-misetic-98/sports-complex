166ae4ced320823636bd908b659f86240fcebcd4
Frane Mišetić <miseticfrane@gmail.com>
2024-11-25 19:05:13 +0100

    Removed class schedule module

--- src/class-application/dto/update-class-application.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateClassApplicationDto } from './create-class-application.dto';
-
-export class UpdateClassApplicationDto extends PartialType(CreateClassApplicationDto) {}
--- src/class-schedule/class-schedule.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ClassScheduleService } from './class-schedule.service';
-import { ClassScheduleController } from './class-schedule.controller';
-
-@Module({
-  controllers: [ClassScheduleController],
-  providers: [ClassScheduleService],
-})
-export class ClassScheduleModule {}
